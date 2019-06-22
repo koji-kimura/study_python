2d739790ce8d40d75240b480bafd5bf0d47c511b
k-kimura <kimura_511@me.com>
2019-06-23 08:57:15 +0900

    about print

--- lesson.py
+++ lesson.py
@@ -1,10 +1,2 @@
-# パイソンの場合は型を指定しないでいい
-num:bool= 1
-
-name:int = "bob"
-
-is_ok:int = True
-
-print(num, type(num))
-print(name, type(name))
-print(is_ok, type(is_ok))
+print('Hi', 'Mike', sep=',', end='')
+print('Hi', 'Mike', sep=',', end='')
