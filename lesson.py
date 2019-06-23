5bd97f649f07280e3b0dee4dad82d95af2cde040
k-kimura <kimura_511@me.com>
2019-06-24 07:31:11 +0900

    strings methods

--- lesson.py
+++ lesson.py
@@ -9,3 +9,24 @@ line3\
 """)
 print('hello'*3)
 print('he''llo')
+
+word = 'python'
+
+word = 'j'+ word[1:]
+print(word)
+print(len(word))
+
+s = 'My name is Mike. Hi Mike.'
+print(s)
+
+is_start = s.startswith('My')
+print(is_start)
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike','Nancy'))
