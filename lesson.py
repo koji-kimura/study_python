5115f6eb93f06dafb9631335e7ad43d03a6162af
k-kimura <kimura_511@me.com>
2019-06-25 08:43:57 +0900

    study tuple

--- lesson.py
+++ lesson.py
@@ -1,14 +1,36 @@
-i = [1,2,3,4,5]
-# 参照渡しになってしまう
-j = i
-i[0] = 100
-print(i)
-print(j)
+# タプルについて
+# ()で囲ってあげる、省略もできる、タプルは,をつける
+t = (1,2,3,4,1,2)
+print(t)
+print(type(t))
+print(t.count(1))
 
-x = [1,2,3,4,5]
-y = x.copy()
-x[0] =100
-print(x)
-print(y)
-print(id(x))
-print(id(y))
+num_tuple = (10,20)
+print(num_tuple)
+x,y = num_tuple
+print(x,y)
+
+# 入れ替えも簡単にできる
+i = 10
+j = 20
+print(i,j)
+tmp = i
+i = j
+j = tmp
+print(i,j)
+
+a = 100
+b = 200
+print(a,b)
+a,b = b,a
+print(a,b)
+
+## ならどうやって使うのか？
+## tuppleは配列とは異なり、appendできないのでその性質を利用する
+chose_from_two = ('A','B','C')
+answer = []
+answer.append('A')
+answer.append('C')
+
+print(chose_from_two)
+print(answer)
