0b8bb8cecf64d5225db0168182b399ad4bc18871
k-kimura <kimura_511@me.com>
2019-06-25 09:17:40 +0900

    study set

--- lesson.py
+++ lesson.py
@@ -1,23 +1,30 @@
-# dictでも参照渡しになってしまう
-x = {'a':1}
-y = x
-y['a'] = 1000
-print(x)
-print(y)
+# 集合について
+# {}でキーをつけないで値を入れればいい
+# 重複が消える
+a = {1,2,3,4,5,5,6}
+b = {2,3,3,6,7}
+# print(a)
+# print(a-b)
+# print(b-a)
+# print(a&b)
+# print(a|b)
+# print(a^b)
 
-x = {'a':1}
-y = x.copy()
-y['a'] = 1000
-print(x)
-print(y)
+# methods
+# s = {1,2,3,4,5}
+# s.add(6)
+# print(s)
+# s.remove(6)
+# print(s)
 
-# どういうタイミングで使うのか？
-# listでもデータ持てるけれど、keyでさっと取ってこれない
-# dictの方が早い
-fruits = {
-  'apple':100,
-  'banana':200,
-  'orange':300
-}
+# どんな時に使うのか?
 
-print('apple %d'%fruits['apple'])
+my_friends = {'A','C','D'}
+A_friends = {'B','D','E','F'}
+
+print(my_friends & A_friends)
+
+# setで集合にできる
+f = ['apple','banana','apple','banana']
+kind = set(f)
+print(kind)
