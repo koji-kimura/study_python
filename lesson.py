4e32524402b683e5690b9f606320000a65f453ac
k-kimura <kimura_511@me.com>
2019-06-26 08:47:05 +0900

    if文の判定について

--- lesson.py
+++ lesson.py
@@ -1,13 +1,7 @@
-y = [1,2,3]
-x = 1
+# is_ok = True
+is_ok = 1
 
-if x in y:
-  print('in')
-
-if 100 not in y:
-  print('not in')
-
-a = 1
-b = 2
-if not a == b:
-  print('Not equel')
+if is_ok:
+  print('OK')
+else:
+  print('No')
