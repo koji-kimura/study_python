3cb99ef5bd86e553fdece9fb34b2836d5dcd9fe0
k-kimura <kimura_511@me.com>
2019-06-26 08:31:58 +0900

    if文

--- lesson.py
+++ lesson.py
@@ -1,30 +1,8 @@
-# 集合について
-# {}でキーをつけないで値を入れればいい
-# 重複が消える
-a = {1,2,3,4,5,5,6}
-b = {2,3,3,6,7}
-# print(a)
-# print(a-b)
-# print(b-a)
-# print(a&b)
-# print(a|b)
-# print(a^b)
+x = 10
 
-# methods
-# s = {1,2,3,4,5}
-# s.add(6)
-# print(s)
-# s.remove(6)
-# print(s)
-
-# どんな時に使うのか?
-
-my_friends = {'A','C','D'}
-A_friends = {'B','D','E','F'}
-
-print(my_friends & A_friends)
-
-# setで集合にできる
-f = ['apple','banana','apple','banana']
-kind = set(f)
-print(kind)
+if x < 10:
+  print('negative')
+elif x == 10:
+  print('10')
+else:
+  print('positive')
