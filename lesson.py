9278435081aa3481dcbff62dd862be3dd2bfb75e
k-kimura <kimura_511@me.com>
2019-06-26 08:43:24 +0900

    inとnot

--- lesson.py
+++ lesson.py
@@ -1,8 +1,13 @@
-x = 10
+y = [1,2,3]
+x = 1
 
-if x < 10:
-  print('negative')
-elif x == 10:
-  print('10')
-else:
-  print('positive')
+if x in y:
+  print('in')
+
+if 100 not in y:
+  print('not in')
+
+a = 1
+b = 2
+if not a == b:
+  print('Not equel')
