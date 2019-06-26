1e156e20d94ba3699eaf70889025eb6e83e92131
k-kimura <kimura_511@me.com>
2019-06-26 09:23:29 +0900

    for else文

--- lesson.py
+++ lesson.py
@@ -1,17 +1,7 @@
-some_list = [1, 2, 3, 4, 5]
-
-# i = 0
-# while i < len(some_list):
-#     print(some_list[i])
-#     i += 1
-
-# for i in some_list:
-#     print(i)
-for s in 'abcde':
-    print(s)
-
-for word in ['My', 'name', 'is', 'Mike']:
-    if word == 'name':
-        # break
-        continue
-    print(word)
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop eating')
+        break
+    print(fruit)
+else:
+    print('I ate all')
