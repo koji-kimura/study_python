7f9945e1303a1396cf12f3ed7f390df121ef7003
k-kimura <kimura_511@me.com>
2019-06-27 08:59:47 +0900

    デフォルト引数での注意点

--- lesson.py
+++ lesson.py
@@ -1,13 +1,30 @@
-def menu(entree="beef", drink="beer", dessert="ice"):
-    print(entree)
-    print(drink)
-    print(dessert)
+# # よくない例
+# def test_func(x, l=[]):
+#     l.append(x)
+#     return l
+
+# リストとか、辞書はデフォルト引数をつけないのが通例
+# この関数みたいにNoneで設定する
 
 
-# この記法だと順番間違えられない
-# menu('beef', 'beer', 'ice')
-# menu(entree='beef',  drink='ice', dessert='beer')
+def test_func(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
 
-menu()
-print("######")
-menu(entree="chiken", drink='beer')
+# y = [1, 2, 3]
+# r = test_func(100, y)
+# print(r)
+
+
+# y = [1, 2, 3]
+# r = test_func(200, y)
+# print(r)
+
+r = test_func(100)
+print(r)
+
+# [100]のみではなく[100,100]になる。これは参照しているから
+r = test_func(100)
+print(r)
