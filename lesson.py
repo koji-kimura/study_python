bc8d488e88912f62a917816b8ce3187ecab85186
k-kimura <kimura_511@me.com>
2019-06-27 08:29:25 +0900

    関数定義

--- lesson.py
+++ lesson.py
@@ -1,6 +1,34 @@
-d = {'x': 100, 'y': 200}
+def say_something():
+    print('hi')
 
-print(d.items())
 
-for k, v in d.items():
-    print(k, ':', v)
+def say_something_return():
+    return 'hi return'
+
+
+say_something()
+
+print(type(say_something))
+
+result = say_something_return()
+print(result)
+
+# 引数の話
+
+
+def what_is_this(color):
+    if(color == 'red'):
+        return 'tomato'
+    elif(color == 'green'):
+        return 'green pepper'
+    else:
+        return "I don't know"
+
+
+color_result = what_is_this('red')
+color_result2 = what_is_this('green')
+color_result3 = what_is_this('blue')
+
+print(color_result)
+print(color_result2)
+print(color_result3)
