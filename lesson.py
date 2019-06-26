407b478b3d1959196643c448b646c2fe37336085
k-kimura <kimura_511@me.com>
2019-06-26 09:09:28 +0900

    input文

--- lesson.py
+++ lesson.py
@@ -1,8 +1,5 @@
-count = 0
-while count < 5:
-    if count == 1:
+while True:
+    word = input('Enter:')
+    if word == 'ok':
         break
-    print(count)
-    count += 1
-else:
-    print('done')
+    print('next')
