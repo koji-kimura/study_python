517fff311cafa6af506c5c8fd6ecd66717c25bd0
k-kimura <kimura_511@me.com>
2019-06-27 08:48:21 +0900

    各種引数について

--- lesson.py
+++ lesson.py
@@ -1,34 +1,13 @@
-def say_something():
-    print('hi')
+def menu(entree="beef", drink="beer", dessert="ice"):
+    print(entree)
+    print(drink)
+    print(dessert)
 
 
-def say_something_return():
-    return 'hi return'
+# この記法だと順番間違えられない
+# menu('beef', 'beer', 'ice')
+# menu(entree='beef',  drink='ice', dessert='beer')
 
-
-say_something()
-
-print(type(say_something))
-
-result = say_something_return()
-print(result)
-
-# 引数の話
-
-
-def what_is_this(color):
-    if(color == 'red'):
-        return 'tomato'
-    elif(color == 'green'):
-        return 'green pepper'
-    else:
-        return "I don't know"
-
-
-color_result = what_is_this('red')
-color_result2 = what_is_this('green')
-color_result3 = what_is_this('blue')
-
-print(color_result)
-print(color_result2)
-print(color_result3)
+menu()
+print("######")
+menu(entree="chiken", drink='beer')
