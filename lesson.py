fbf172545eaad3366a0b3e10deebd8651c6edc84
k-kimura <kimura_511@me.com>
2019-06-26 09:06:29 +0900

    while文

--- lesson.py
+++ lesson.py
@@ -1,12 +1,8 @@
 count = 0
-# while count < 5:
-#   print(count)
-#   count += 1
-while True:
-    if count >= 5:
+while count < 5:
+    if count == 1:
         break
-    if count == 2:
-        count += 1
-        continue
     print(count)
     count += 1
+else:
+    print('done')
