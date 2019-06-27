5c9027d33a92cbbaa2dae4d97075ea17625b0131
k-kimura <kimura_511@me.com>
2019-06-27 09:21:57 +0900

    キーワード引数の辞書化

--- lesson.py
+++ lesson.py
@@ -1,11 +1,29 @@
-# 引数の頭に*を入れることで引数をタプル化できる
-def say_something(word, *args):
-    print('word=', word)
-    for arg in args:
-        print(arg)
+# 引数もっと追加したい
+# def menu(entree='beef', drink='wine'):
+#     print(entree, drink)
+
+# **で辞書化できる
 
 
-say_something('hi', 'Mike', 'Nance')
+# def menu(**kwargs):
+#     # print(kwargs)
+#     for k, v in kwargs.items():
+#         print(k, v)
 
-# t = ('Mike', 'Nancy')
-# say_something('hi', *t)
+# 引数を文字列タプル辞書と併用できる
+def menu(food, *args, **kwargs):
+    # print(kwargs)
+    print(food)
+    print(args)
+    print(kwargs)
+
+
+# menu(entree="beef", drink="coffee")
+d = {
+    'entree': 'beef',
+    'drink': 'ice coffee',
+    'dessert': 'ice'
+}
+# **で辞書を展開している
+# menu(**d)
+menu('banana', 'appe', 'orange', entree="beef", drink="coffee")
