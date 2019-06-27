40f7ed87aa807c52ae172e033269272a5e14e88c
k-kimura <kimura_511@me.com>
2019-06-28 08:45:21 +0900

    dogstrings

--- lesson.py
+++ lesson.py
@@ -1,29 +1,11 @@
-# 引数もっと追加したい
-# def menu(entree='beef', drink='wine'):
-#     print(entree, drink)
-
-# **で辞書化できる
-
-
-# def menu(**kwargs):
-#     # print(kwargs)
-#     for k, v in kwargs.items():
-#         print(k, v)
-
-# 引数を文字列タプル辞書と併用できる
-def menu(food, *args, **kwargs):
-    # print(kwargs)
-    print(food)
-    print(args)
-    print(kwargs)
+def example_func(p1, p2):
+    """
+      test
+      dogstringsのための記述
+    """
+    print(p1)
+    print(p2)
+    return True
 
 
-# menu(entree="beef", drink="coffee")
-d = {
-    'entree': 'beef',
-    'drink': 'ice coffee',
-    'dessert': 'ice'
-}
-# **で辞書を展開している
-# menu(**d)
-menu('banana', 'appe', 'orange', entree="beef", drink="coffee")
+print(example_func.__doc__)
