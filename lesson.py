a5b0444840ed5a9ad237e1ee839b45cd5a24df50
k-kimura <kimura_511@me.com>
2019-06-28 09:02:25 +0900

    クロージャー

--- lesson.py
+++ lesson.py
@@ -1,11 +1,23 @@
-def example_func(p1, p2):
-    """
-      test
-      dogstringsのための記述
-    """
-    print(p1)
-    print(p2)
-    return True
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
 
 
-print(example_func.__doc__)
+ca1 = circle_area_func(3.14)
+ca2 = circle_area_func(3.1415)
+
+print(ca1(10))
+print(ca2(10))
+
+# def outer(a, b):
+#     def inner():
+#         return a+b
+#     # 関数を返しているだけ
+#     return inner
+
+
+# f = outer(1, 2)
+# r = f()
+# print(r)
