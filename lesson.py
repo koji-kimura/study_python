43eb4daec035e4655005355c3113536fe2360b91
k-kimura <kimura_511@me.com>
2019-06-30 10:48:46 +0900

    importでtry・error文

--- lesson.py
+++ lesson.py
@@ -2,9 +2,15 @@
 # from lesson_package.tools import utils
 # from lesson_package.talk import human
 # from lesson_package.talk import animal
-from lesson_package.talk import *
+# from lesson_package.talk import *
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
+
+try:
+    from lesson_package import *
+except ImportError:
+    from lesson_package.talk import *
+
 print(animal.sing())
 print(animal.cry())
 
