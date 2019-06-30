75be02b2530c406d2cfe7ad69b7e01988a4fa42e
k-kimura <kimura_511@me.com>
2019-06-30 11:13:32 +0900

    標準ライブラリ

--- lesson.py
+++ lesson.py
@@ -1,13 +1,25 @@
-# print()、next()も組み込み関数
-print(globals())
+from collections import defaultdict
+s = "fhlsdkfhksdhgksdh;lsdhh;fdgh;fdhg"
 
-ranking = {
-    'A': 100,
-    'B': 85,
-    'C': 95,
-}
-# 配列.get(key)で値をとる
-print(ranking.get('A'))
+d = {}
 
-# getで取ってきた値をキーにしてソートをかける
-print(sorted(ranking, key=ranking.get, reverse=True))
+# for c in s:
+#     if c not in d:
+#         d[c] = 0
+#     d[c] += 1
+
+# print(d)
+
+# for c in s:
+#     # 辞書にはデフォルト値を設定できる
+#     d.setdefault(c, 0)
+#     d[c] += 1
+
+# 上記のようにしなくても組み込み関数でできる
+d = defaultdict(int)
+for c in s:
+    d[c] += 1
+
+print(d)
+
+print(d['f'])
