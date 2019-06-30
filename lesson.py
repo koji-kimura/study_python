45d99264581145a49c9c3885705fda47585e1d8c
k-kimura <kimura_511@me.com>
2019-06-30 10:44:55 +0900

    __all__の使い方について

--- lesson.py
+++ lesson.py
@@ -1,9 +1,11 @@
 # import lesson_package.utils
-from lesson_package.tools import utils
-from lesson_package.talk import human
+# from lesson_package.tools import utils
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+from lesson_package.talk import *
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
-r = utils.say_twice('hello')
+print(animal.sing())
+print(animal.cry())
 
-print(r)
-print(human.sing())
+# print(human.sing())
