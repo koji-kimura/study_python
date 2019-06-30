e13a2bcc06f5d041134e4e1fd8a6fe83cb991fee
k-kimura <kimura_511@me.com>
2019-06-30 11:23:39 +0900

    サードパーティーのライブラリ

--- lesson.py
+++ lesson.py
@@ -1,25 +1,6 @@
-from collections import defaultdict
-s = "fhlsdkfhksdhgksdh;lsdhh;fdgh;fdhg"
+#  pipでpypiにあるサードパーティーのライブラリをインストールできる
+# インストールはpip install
+from termcolor import colored
 
-d = {}
-
-# for c in s:
-#     if c not in d:
-#         d[c] = 0
-#     d[c] += 1
-
-# print(d)
-
-# for c in s:
-#     # 辞書にはデフォルト値を設定できる
-#     d.setdefault(c, 0)
-#     d[c] += 1
-
-# 上記のようにしなくても組み込み関数でできる
-d = defaultdict(int)
-for c in s:
-    d[c] += 1
-
-print(d)
-
-print(d['f'])
+print(colored('test', 'green'))
+print(help(colored))
