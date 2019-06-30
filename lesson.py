5fcfba7f64ab3fe25ad722a9b5c9863996c17191
k-kimura <kimura_511@me.com>
2019-06-30 10:33:04 +0900

    importについて

--- lesson.py
+++ lesson.py
@@ -1,6 +1,9 @@
 # import lesson_package.utils
-from lesson_package import utils as u
+from lesson_package.tools import utils
+from lesson_package.talk import human
 # r = lesson_package.utils.say_twice('hello')
 # r = utils.say_twice('hello')
-r = u.say_twice('hello')
+r = utils.say_twice('hello')
+
 print(r)
+print(human.sing())
