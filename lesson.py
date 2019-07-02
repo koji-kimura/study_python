103aa067a639617fe1c80d400bb872da5bf1ee72
k-kimura <kimura_511@me.com>
2019-07-02 13:02:54 +0900

    メソッドのオーバーライド

--- lesson.py
+++ lesson.py
@@ -1,13 +1,24 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
     def run(self):
         print('run')
 
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
 
 class TeslaCar(Car):
+    def __init__(self, model="Model S", enable_auto_run=False):
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+    def run(self):
+        print('super fast')
+
     def auto_run(self):
         print('auto run')
 
@@ -15,10 +26,17 @@ class TeslaCar(Car):
 car = Car()
 car.run()
 
+print("#"*10)
+
+
 # 継承
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 
-tesla_car = TeslaCar()
+print("#"*10)
+
+tesla_car = TeslaCar('model_S')
+print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
