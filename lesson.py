85c24db3738b6aa3e188a6394bf9b39115c62dc4
k-kimura <kimura_511@me.com>
2019-07-02 12:39:43 +0900

    クラスの初期化とクラス変数

--- lesson.py
+++ lesson.py
@@ -1,8 +1,19 @@
 # class Person(object):のobjectは省略できる
 class Person(object):
+    def __init__(self, name):
+        self.name = name
+        # print('First', name)
+
     def say_somethig(self):
-        print('hello')
+        # initで入れたものは他のメソッドの中でも使える
+        print('I am {}.hello'.format(self.name))
+        # 自分自身の中から呼び出せる
+        # selfは引数をとらないのでこの記述でOK
+        self.run(10)
+
+    def run(self, num):
+        print('run' * num)
 
 
-person = Person()
+person = Person('mike')
 person.say_somethig()
