50b69be578700dbaed51d055762f9f0efb1c600a
k-kimura <kimura_511@me.com>
2019-07-02 12:51:28 +0900

    クラスの継承

--- lesson.py
+++ lesson.py
@@ -1,26 +1,24 @@
-# class Person(object):のobjectは省略できる
-class Person(object):
-    # コンストラクタ
-    def __init__(self, name):
-        self.name = name
-        # print('First', name)
-
-    def say_somethig(self):
-        # initで入れたものは他のメソッドの中でも使える
-        print('I am {}.hello'.format(self.name))
-        # 自分自身の中から呼び出せる
-        # selfは引数をとらないのでこの記述でOK
-        self.run(10)
-
-    def run(self, num):
-        print('run' * num)
-
-    # デストラクタ
-    def __del__(self):
-        print('good bye')
+class Car(object):
+    def run(self):
+        print('run')
 
 
-person = Person('mike')
-person.say_somethig()
-# 明示的に呼びたい場合
-del person
+class ToyotaCar(Car):
+    pass
+
+
+class TeslaCar(Car):
+    def auto_run(self):
+        print('auto run')
+
+
+car = Car()
+car.run()
+
+# 継承
+toyota_car = ToyotaCar()
+toyota_car.run()
+
+tesla_car = TeslaCar()
+tesla_car.run()
+tesla_car.auto_run()
