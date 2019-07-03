17989a8ecf0129d0afa2dff83e10f61729c24a90
k-kimura <kimura_511@me.com>
2019-07-03 21:36:20 +0900

    プロパティを使った属性の設定

--- lesson.py
+++ lesson.py
@@ -12,9 +12,26 @@ class ToyotaCar(Car):
 
 
 class TeslaCar(Car):
-    def __init__(self, model="Model S", enable_auto_run=False):
+    def __init__(self, model="Model S",
+                 enable_auto_run=False,
+                 passwd='123'):
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run
+        # アンダースコア2こならクラス内でしかいじれない
+        self.__enable_auto_run = enable_auto_run
+        # 初期値に設定する必要あり
+        self.passwd = passwd
+    # この記述で読み込み専用になる
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    # setterで書き換えできるようになる
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
 
     def run(self):
         print('super fast')
@@ -23,20 +40,8 @@ class TeslaCar(Car):
         print('auto run')
 
 
-car = Car()
-car.run()
-
 print("#"*10)
 
-
-# 継承
-toyota_car = ToyotaCar('Lexus')
-print(toyota_car.model)
-toyota_car.run()
-
-print("#"*10)
-
-tesla_car = TeslaCar('model_S')
-print(tesla_car.model)
-tesla_car.run()
-tesla_car.auto_run()
+tesla_car = TeslaCar('Lexus', passwd='456')
+tesla_car.enable_auto_run = True
+print(tesla_car.enable_auto_run)
