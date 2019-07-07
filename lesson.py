2098c9773ac397312df7f0678be5a196acad2984
k-kimura <kimura_511@me.com>
2019-07-08 07:57:07 +0900

    ファイルの読み込み

--- lesson.py
+++ lesson.py
@@ -1,3 +1,19 @@
-# f = open('test.txt', 'w')
-with open('test.txt', 'w') as f:
-    print('\n print', file=f)
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
+# with open('test.txt', 'w') as f:
+#     f.write(s)
+
+with open('test.txt', 'r') as f:
+    # print(f.read())
+    while True:
+        # line = f.readline()
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
