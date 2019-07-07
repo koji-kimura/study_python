13316aa9a61ea3204d133b6564b3dd9b29ed075b
k-kimura <kimura_511@me.com>
2019-07-08 07:48:09 +0900

    ファイルの作成

--- lesson.py
+++ lesson.py
@@ -1,22 +1,5 @@
-class Word(object):
-    # __があるものが特殊メソッド
-    def __init__(self, text):
-        self.text = text
-
-    def __str__(self):
-        return 'word!!!'
-
-    def __len__(self):
-        return len(self.text)
-
-    def __add__(self, word):
-        return self.text.lower() + word.text.lower()
-
-    def __eq__(self, word):
-        return self.text.lower() == word.text.lower()
-
-
-w = Word('test')
-w2 = Word('####')
-
-print(w == w2)
+# f = open('test.txt', 'w')
+f = open('test.txt', 'a')
+# flie = fを入れないと普通にコンソールに吐き出されるだけ
+print('\n print', file=f)
+f.close()
