5741a56d11c6a00d4f758b2cd12fa80691d2ef65
k-kimura <kimura_511@me.com>
2019-07-18 08:43:32 +0900

    logging

--- lesson.py
+++ lesson.py
@@ -1,33 +1,12 @@
-import os
-import time
-import datetime
-import shutil
+import logging
 
-now = datetime.datetime.now()
-print(now)
-print(now.isoformat())
-print(now.strftime('%d/%m/%y-%H'))
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
 
-today = datetime.date.today()
-print(today)
-print(today.isoformat())
+logging.critical('critical')
+logging.error('error')
+logging.debug('debug')
+logging.info('info')
+logging.debug('debug')
 
-t = datetime.time(hour=1, minute=10)
-print(t)
-
-print(now)
-d = datetime.timedelta(weeks=1)
-print(now - d)
-
-# print('##')
-# time.sleep(2)
-# print('##')
-print(time.time())
-
-
-file_name = 'test.txt'
-if os.path.exists(file_name):
-    shutil.copy(file_name, "{}.{}".format(file_name, now))
-
-with open(file_name, 'w') as f:
-    f.write('test')
+logging.info('info{}'.format('test'))
+logging.info('info %s %s', 'test1', 'test2')
