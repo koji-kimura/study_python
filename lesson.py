d929a25d2f190073bd6c5add65b61e3331dfe958
k-kimura <kimura_511@me.com>
2019-07-20 14:07:00 +0900

    ロギング　フィルタ

--- lesson.py
+++ lesson.py
@@ -4,8 +4,20 @@ import logtest
 # formatter = '%(levelname)s:%(message)s'
 formatter = '%(asctime)s:%(message)s'
 logging.basicConfig(level=logging.INFO)
+
+
+class NoPassFilter(logging.Filter):
+    def filter(self, record):
+        log_message = record.getMessage()
+        return 'password' not in log_message
+
+
 logging.info('info')
 logger = logging.getLogger(__name__)
+logger.addFilter(NoPassFilter())
+
 logger.info('from main')
+logger.info('from main password="test"')
+logger.info('from main passwod="test"')
 # logger.setLevel(logging.DEBUG)
-logtest.do_something()
+# logtest.do_something()
