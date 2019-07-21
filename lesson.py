4423d9c6269e609944f61fb1a2626fd851fdaeeb
k-kimura <kimura_511@me.com>
2019-07-21 19:27:45 +0900

    Eメール送信

--- lesson.py
+++ lesson.py
@@ -1,9 +1,24 @@
-import loggin.config
+from email import message
+import smtplib
 
-logging.config.fileConfig('logging.oini')
-logger = logging.getLogger(__name__)
+smtp_host = 'smtp.live.com'
+smtp_port = 587
+# 捨てアカウント
+to_email = 'npug310ghhqp@sute.jp'
+from_email = 'npug310ghhqp@sute.jp'
+username = 'test'
+password = 'test'
 
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.critival('critival message')
+msg = message.EmailMessage()
+msg.set_content('test email')
+msg['Subject'] = 'test email sub'
+msg['From'] = from_email
+msg['To'] = to_email
+
+server = smtplib.SMTP(smtp_host, smtp_port)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.send.message(msg)
+server.quit()
