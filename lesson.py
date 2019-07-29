5cb68a1ad6b8d8a47e46499c4aecaf19522a845c
k-kimura <kimura_511@me.com>
2019-07-29 21:52:07 +0900

    DBM

--- lesson.py
+++ lesson.py
@@ -1,36 +1,8 @@
-import sqlalchemy
-import sqlalchemy.ext.declarative
-import sqlalchemy.orm
+import dbm
 
-engine = sqlalchemy.create_engine('sqlite:///test_sqlite2', echo=True)
+with dbm.open('cache', 'c') as db:
+    db['key1'] = 'value1'
+    db['key2'] = 'value2'
 
-Base = sqlalchemy.ext.declarative.declarative_base()
-
-
-class Person(Base):
-    __tablename__ = 'persons'
-    id = sqlalchemy.Column(
-        sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String(14))
-
-
-Base.metadata.create_all(engine)
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-session = Session()
-
-p1 = Person(name='Mike')
-p2 = Person(name='Nancy')
-p3 = Person(name='Jun')
-session.add(p1)
-session.add(p2)
-session.add(p3)
-session.commit()
-
-p4 = session.query(Person).filter_by(name='Mike').first()
-p4.name = 'Micel'
-session.query(Person)
-session.add(p4)
-session.commit()
-persons = session.query(Person).all()
-for person in persons:
-    print(person.id, person.name)
+with dbm.open('cache', 'r') as db:
+    print(db.get('key1'))
