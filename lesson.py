b15c8c59b38e90b7246ff53a209cd320822f46f3
k-kimura <kimura_511@me.com>
2019-07-31 10:55:33 +0900

    pickle

--- lesson.py
+++ lesson.py
@@ -1,8 +1,25 @@
-import dbm
+import pickle
 
-with dbm.open('cache', 'c') as db:
-    db['key1'] = 'value1'
-    db['key2'] = 'value2'
 
-with dbm.open('cache', 'r') as db:
-    print(db.get('key1'))
+class T(object):
+    def __init__(self, name):
+        self.name = name
+
+
+data = {
+    'a': [1, 2, 3],
+    'b': {'test', 'test'},
+    'c': {'key': 'value'},
+    'd': T('test')
+}
+
+with open('data.pickle', 'wb') as f:
+    pickle.dump(data, f)
+
+with open('data.pickle', 'rb') as f:
+    data_loaded = pickle.load(f)
+    print(data_loaded['d'].name)
+    print(type(data_loaded['a']))
+    print(type(data_loaded['b']))
+    print(type(data_loaded['c']))
+    print(type(data_loaded['d']))
