c12fc8babe04d39e56f4f771f19a89c4db7d5b89
k-kimura <kimura_511@me.com>
2019-08-03 14:01:00 +0900

    json

--- lesson.py
+++ lesson.py
@@ -1,26 +1,18 @@
-import xml.etree.ElementTree as ET
-root = ET.Element('root')
-tree = ET.ElementTree(element=root)
+import json
+j = {
+    "employee":
+    [
+        {"id": 111, 'name': "Mike"},
+        {"id": 222, 'name': "Nancy"}
+    ]
+}
 
-employee = ET.SubElement(root, 'employee')
-employ = ET.SubElement(employee, 'employ')
-employ_id = ET.SubElement(employ, 'id')
-employ_id.text = '111'
-employ_id = ET.SubElement(employ, 'name')
-employ_id.text = 'Mike'
+print(j)
+print("#####")
+print(json.dumps(j))
 
-employ = ET.SubElement(employee, 'employ')
-employ_id = ET.SubElement(employ, 'id')
-employ_id.text = '222'
-employ_id = ET.SubElement(employ, 'name')
-employ_id.text = 'Nancy'
+with open('test.json', 'w') as f:
+    json.dump(j, f)
 
-tree.write('test.xml', encoding="utf-8", xml_declaration=True)
-
-tree = ET.ElementTree(file='test.xml')
-root = tree.getroot()
-
-for employee in root:
-    for employ in employee:
-        for person in employ:
-            print(person.tag, person.text)
+with open('test.json', 'r') as f:
+    print(json.load(f))
